a1428568632927c3f97c3ef8d67ab4adb4c44be9
kasey.culmback <kasey.culmback@aon.com>
2020-08-10 17:37:43 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
+};
