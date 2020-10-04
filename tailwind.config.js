b877f023def006c3894ae8598c592e03cd4f0077
kasey.culmback <kasey.culmback@aon.com>
2020-10-04 12:25:36 -0700

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./src/**/*.html'],
+  purge: ['./src/**/*.html', './src/**/*.pug'],
   theme: {
     extend: {
       fontFamily: {
