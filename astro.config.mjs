132def2a880a1d5b960c088defb8d193b3e9e193
Kasey Culmback <kasey.culmback@gmail.com>
2025-06-02 12:25:22 -0700

    set up vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercelStatic({}),
   vite: {
     plugins: [tailwindcss()],
   },
